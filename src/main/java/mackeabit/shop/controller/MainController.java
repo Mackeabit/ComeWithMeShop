a198712a77bdbdfc60d29af2240a22eaf79b6473
Mackeabit <thdrudgh7775@naver.com>
2023-02-08 18:49:51 +0900

    프로젝트 시작

---
+++ src/main/java/mackeabit/shop/controller/MainController.java
@@ -0,0 +1,14 @@
+package mackeabit.shop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+    @RequestMapping("/")
+    public String basic() {
+        return "index";
+    }
+}
