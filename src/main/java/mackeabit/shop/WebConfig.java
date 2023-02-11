98e2f0bf3e376aa3c807df9ade6776a34edd8023
Mackeabit <thdrudgh7775@naver.com>
2023-02-11 18:56:29 +0900

    - 인터셉터 설정 추가(exclude 미완)

---
+++ src/main/java/mackeabit/shop/WebConfig.java
@@ -0,0 +1,25 @@
+package mackeabit.shop;
+
+import mackeabit.shop.interceptor.LogInterceptor;
+import mackeabit.shop.interceptor.LoginCheckInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/**", "/*.ico", "/error");
+
+        registry.addInterceptor(new LoginCheckInterceptor())
+                .order(2)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/"); //추후 예외 위치 추가
+    }
+}
